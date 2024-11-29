f77a4a81e129a0d61878e1a1583c1153d503bcc5
ElizaLara99 <larabeth94@gmail.com>
2024-11-29 15:44:16 -0600

    ejercicio de LiveCoding

--- starter-files/script.js
+++ starter-files/script.js
@@ -3,11 +3,20 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
 
 /**
  * STEPS:
- *
  * 1. Create a fetchAnswer function and call the API
  * 2. Output the API's response
  * 3. Attach fetchAnswer to an event listener
  * 4. Clear output after 3 seconds
  * 5. Optional: add loading/error states
- *
  */
+//creando funcion fetchAnswer
+async function fetchAnswer() {
+    const input = document.getElementById('input');
+    const answerDiv = document.getElementById('answer');
+    const errorDiv = document.getElementById('error');
+    errorDiv.textContent = '';
+
+    if (input.value.trim() === '') {
+        errorDiv.textContent = 'Haz una pregunta.';
+        return;
+    }
